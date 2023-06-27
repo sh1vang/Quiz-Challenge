422d45008a54bfa0f36343bb220cecd86fca9bae
sh1vang <136671287+sh1vang@users.noreply.github.com>
2023-06-27 07:56:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+const quizDB = [
+        {
+            question: "Q1: What is the full form of HTML?",
+            a: "Hello To My Land",
+            b: "Hey Text Markup Language",
+            c: "HyperText Makeup Language",
+            d: "Hypertext Markup Language",
+            ans: "ans4"
+          },
+            {
+            
+            question: "Q2: What is the full form of CSS?", 
+            a: "Cascading Style Sheets",
+            b: "Cascading Style Sheep",
+            c: "Cartoon Style Sheets",
+            d: "Cascading Super Sheets",
+            ans: "ans1"
+            },
+            {
+            question: "Q3: What is the full form of HTTP?",
+            a: "Hypertext Transfer Product",
+            b: "Hypertext Test Protocol",
+            c: "Hey Transfer Protocol",
+            d: "Hypertext Transfer Protocol",
+            ans: "ans4"
+          },
+          {
+            question: "Q4: What is the full form of JS?",
+            a: "JavaScript",
+            b: "JavaSuper",
+            c: "JustScript",
+            d: "JordenShoes",
+            ans: "ans1"
+             }
+        ];
+
+const question=document.querySelector('.question');
+const option1=document.querySelector('#option1');
+const option2=document.querySelector('#option2');
+const option3=document.querySelector('#option3');
+const option4=document.querySelector('#option4');
+const submit=document.querySelector('#submit');
+
+const answers =document.querySelectorAll('.answer');
+const showscore=document.querySelector('#showscore');
+
+let questionCount=0;
+let score=0;
+const loadQuestion = () => {
+    const questionList=quizDB[questionCount];
+    question.innerText=questionList.question;  
+    option1.innerText=questionList.a;
+    option2.innerText=questionList.b;
+    option3.innerText=questionList.c;
+    option4.innerText=questionList.d;
+};
+loadQuestion();
+let curAnsElem;
+const getCheckAnswer=() => {
+    let answer;
+   
+    answers.forEach((curAnsElem) => {
+        if(curAnsElem.checked){
+        answer=curAnsElem.id;
+        }
+    });
+    return answer;
+};
+
+const deselectAll = () => {
+   answers.forEach((curAnsElem) => curAnsElem.checked=false);
+}
+submit.addEventListener('click', () => {
+    const checkedAnswer= getCheckAnswer();
+    console.log(checkedAnswer);
+
+    if(checkedAnswer===quizDB[questionCount].ans){
+        score=score+1;
+    };
+
+    questionCount=questionCount+1;
+
+    deselectAll();
+    if(questionCount<quizDB.length){
+       loadQuestion();
+    }else{
+        showscore.innerHTML=`
+        <h3> You scored ${score}/${quizDB.length}✌ <\h3>
+        <button class="btn" onclick="location.reload()"> Play Again </button>
+        `;
+
+        showscore.classList.remove('scorearea');
+    }
+});
